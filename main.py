de6763424bc3f98a432b07378bab32fd3f3b7fe5
yuan <a0492651586@gmail.com>
2025-06-07 23:44:28 +0800

    ?

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from routers import chat
 from routers import weather
+from routers import lalo
 
 app = FastAPI()
 app.include_router(chat.router)
 app.include_router(weather.router)
+app.include_router(lalo.router)
